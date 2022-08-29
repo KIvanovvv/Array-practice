c52c0d9f56f177da6ef54aa0882cb318e0e618d4
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-08-29 15:53:44 +0100

    Method forEach practice

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -106,15 +106,34 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // //Join
 // console.log(letters.join(` - `));
 
-//At
-const arr = [23, 11, 64];
-console.log(arr[0]);
-console.log(arr.at(0));
+// //At
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
 
-//Getting the last array element
-console.log(arr[arr.length - 1]);
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
+// //Getting the last array element
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
 
-console.log(`jonas`.at(0));
-console.log(`jonas`.at(-1));
+// console.log(`jonas`.at(0));
+// console.log(`jonas`.at(-1));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//for (const movement of movements)
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} :You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1} You withdraw ${Math.abs(movement)}`);
+  }
+}
+console.log(`---------`);
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1} :You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1} You withdraw ${Math.abs(mov)}`);
+  }
+});
