96eaf0ffbc85191576d02b88001c564e4eaa57d4
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-05 19:16:41 +0100

    Practice "every" Method and seperate callback functions

--- script.js
+++ script.js
@@ -178,3 +178,12 @@ console.log(movements.includes(-130));
 
 const anyDeposits = movements.some(mov => mov > 1500);
 console.log(anyDeposits);
+//Every
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+//Seperate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
