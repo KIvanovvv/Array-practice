29673e2b2f5b8461c3e3fb81b83dac3aece009ff
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-06 17:34:18 +0100

    sort Method

--- script.js
+++ script.js
@@ -214,3 +214,30 @@ const overalBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov);
 console.log(overalBalance2);
+
+//Sort
+
+//String
+const owners = [`Jonas`, `Zach`, `Adam`, `Martha`];
+console.log(owners.sort());
+
+//Numbers
+console.log(movements);
+
+//return <0,A,B(keep order)
+//return >0,B,A(switch order)
+
+//Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (b > a) return -1;
+// });
+movements.sort((a, b) => a - b);
+//Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (b > a) return 1;
+// });
+movements.sort((a, b) => b - a);
+
+console.log(movements);
